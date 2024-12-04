40d0fca56a0f52631c05f53871214062bd31b72a
harusame1018 <163406621+harusame1018@users.noreply.github.com>
2024-12-04 04:50:31 +0000

    changes

---
+++ discord.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client(
+  { intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+    ]
+  }
+);
+
+client.on('messageCreate', message => {
+  if(message.author.bot) return; //BOTのメッセージには反応しない
+
+  if(message.content === "こんにちわ") {
+    message.channel.send("こんにちわ！");
+  }
+});
+
+client.on('ready', () => {
+  console.log('ボットが起動したよ');
+});
+client.login("");
